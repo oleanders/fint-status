95a12b8f4e0930555157bd89adc68709cc99c37f
Ole Anders Eidjord <oa@eidjord.no>
2023-09-11 21:01:15 +0200

    function proxy

--- src/Main.elm
+++ src/Main.elm
@@ -489,9 +489,10 @@ httpRequest environment url msg decoder =
                 ++ headerAuth
         , url =
             --if environment == PlayWithFint then
-            completeUrl
+            --completeUrl
+            --else
+            "https://us-central1-eidjord.cloudfunctions.net/proxy/" ++ String.replace "https://play-with-fint.felleskomponent.no/" "" url
 
-        --else
         --"http://localhost:8010/proxy/" ++ String.replace "https://play-with-fint.felleskomponent.no/" "" url
         , body = Http.emptyBody
         , expect = Http.expectJson msg decoder
